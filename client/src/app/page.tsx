55e77fe3e241b192b515a7421075138484278550
asifakhtar18 <mdasifakhtar1808@gmail.com>
2024-08-01 01:39:28 +0530

    fix

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React from "react";
-import type { AppProps } from "next/app";
 import { ThemeProvider } from "@material-tailwind/react";
 import ReduxProvider from "../store/reduxProvider";
 import "./globals.css";
 
-const Home: React.FC<AppProps> = ({ Component, pageProps }) => {
+const Home: React.FC<> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
       <ReduxProvider>
